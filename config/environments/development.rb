0fbd19ee39162ba175f2a147c9f3504befeac744
SGhyun <sghiroo@sogang.ac.kr>
2017-05-14 15:16:00 +0000

    back up

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,9 +27,8 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.perform_caching = false
+  # config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -44,4 +43,9 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+
+  # 디버깅 정보 제공 형식
+  # :default는 api 형식
+  # config.debug_exception_response_format = :default
 end
