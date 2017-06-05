fedca5362255a6d81f139cbf4e96073f2231063f
SGhyun <sghiroo@sogang.ac.kr>
2017-06-05 08:22:29 +0000

    pwd hide

--- emit_log.rb
+++ emit_log.rb
@@ -5,7 +5,7 @@ require "bunny"
 require 'json'
 
 # conn = Bunny.new("amqp://ssumtago:Tjaxkrh@127.0.0.1")
-conn = Bunny.new(:host => "expirit.co.kr", :vhost => "pushHost", :user => "ssumtago", :password => "Tjaxkrh")
+conn = Bunny.new(:host => "expirit.co.kr", :vhost => "pushHost", :user => "ssumtago", :password => ENV["RabbitMQ_pwd"])
 conn.start
 
 ch   = conn.create_channel
