7758eff49e867a6294e402ce8a580eb897b6687b
SGhyun <sghiroo@sogang.ac.kr>
2017-06-05 08:08:58 +0000

    vhost chagne

--- emit_log.rb
+++ emit_log.rb
@@ -4,7 +4,8 @@
 require "bunny"
 require 'json'
 
-conn = Bunny.new("amqp://ssumtago:Tjaxkrh@127.0.0.1")
+# conn = Bunny.new("amqp://ssumtago:Tjaxkrh@127.0.0.1")
+conn = Bunny.new(:host => "localhost", :vhost => "pushHost", :user => "ssumtago", :password => "Tjaxkrh")
 conn.start
 
 ch   = conn.create_channel
@@ -16,3 +17,10 @@ ch.default_exchange.publish( requestSurvey.to_json, :routing_key => q.name )
 puts " [x] Sent #{requestSurvey.to_json}"
 
 conn.close
+
+# begin
+#   conn = Bunny.new("amqp://guest8we78w7e8:guest2378278@127.0.0.1")
+#   conn.start
+# rescue Bunny::PossibleAuthenticationFailureError => e
+#   puts "Could not authenticate as #{conn.username}"
+# end
