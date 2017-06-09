01141bcadbbad5b57908d5baa61351fb7f29625a
SGhyun <sghiroo@sogang.ac.kr>
2017-06-09 16:15:02 +0000

    [:user] 삭제 수정

--- app/models/ssumji.rb
+++ app/models/ssumji.rb
@@ -1,7 +1,7 @@
 class Ssumji
   include Mongoid::Document
   # Mongoid.embedded_object_id = false
-  object_id false
+  # object_id false
   field :questionCode, as: :question_code, type: String
   field :answerCode, as: :answer_code, type: String
   embedded_in :report
