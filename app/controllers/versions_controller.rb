827e465b35e4753b49f0803e696f1cc4fb1979c1
SGJunghyun <sghiroo@sogang.ac.kr>
2017-08-22 03:28:11 +0900

    version Retrun

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -2,9 +2,10 @@ class VersionsController < ApplicationController
   def versionsCheck
     logger.info "[LINE:#{__LINE__}] version 리턴완료 / 통신종료"
     @versions = {
-      androidVersion:"1.0.0",
-      iosVersion:"1.0.3",
-      dbVersion:"1.0.0"
+      androidVersion: Rails.configuration.x.android_app_version,
+      iosVersion: Rails.configuration.x.ios_app_version,
+      serverVersion: Rails.configuration.x.server_app_version,
+      dbVersion: Rails.configuration.x.db_version
     }
     render json: @versions, status: :ok
   end
